303fbc7d8f4c1c6393311a4ce368d512f8438125
Jeff <admin@jeffwilliams.dev>
2021-02-07 16:11:36 -0700

    MVP-66 wrench: suspended store for testing

--- store/reports.js
+++ store/reports.js
@@ -1,15 +1,15 @@
 export const state = () => ({
-    rewardingRating: Number,
-    perceivedExertion: Number,
-    painLevel: Number,
-    // duration of activity in min
-    time: Number,
-    // don't know how to value intensity yet
-    intensity: Number,
-    symptoms: [],
-    // created, submitted to provider, reviewed by provider
-    state: 'created',
-    activity: '',
+    // rewardingRating: Number,
+    // perceivedExertion: Number,
+    // painLevel: Number,
+    // // duration of activity in min
+    // time: Number,
+    // // don't know how to value intensity yet
+    // intensity: Number,
+    // symptoms: [],
+    // // created, submitted to provider, reviewed by provider
+    // state: 'created',
+    // activity: '',
 })
 
 export const getters = {
